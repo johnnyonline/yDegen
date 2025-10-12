2055e3545d64b27e4c642aff2e6dfaabedb47cdc
johnnyonline <johnnyonline.eth@gmail.com>
2025-10-11 21:48:30 -0300

    chore: increase alert cooldown

--- bot/bot.py
+++ bot/bot.py
@@ -24,7 +24,7 @@ bot = SilverbackBot()
 STATE_FILE = "bot_state.json"
 
 STATUS_REPORT_CRON = os.getenv("STATUS_REPORT_CRON", "0 8 * * *")  # Daily at 8 AM UTC
-ALERT_COOLDOWN_SECONDS = int(os.getenv("TEND_TRIGGER_ALERT_COOLDOWN_SECONDS", "3600"))  # 1 hour default
+ALERT_COOLDOWN_SECONDS = int(os.getenv("TEND_TRIGGER_ALERT_COOLDOWN_SECONDS", "86400"))  # 24 hours default
 
 
 # =============================================================================
