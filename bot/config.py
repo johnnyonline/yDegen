ff32d72309db662109a9bd7b9306957748220071
johnnyonline <johnnyonline.eth@gmail.com>
2026-01-18 19:30:50 -0300

    chore: update crvusd lb addresses

--- bot/config.py
+++ bot/config.py
@@ -23,7 +23,7 @@ NETWORKS: Mapping[str, NetworkCfg] = {
         "lender_borrowers": [
             "0x6dec370EfA894d48D8C55012B0Cd6f3C1C7C4616",  # Asymmetry tBTC Lender USDaf Borrower
             "0x48E560AfB1482f63e5e8C0eA7fBdd8Fd82054eC3",  # Curve WETH Lender crvUSD Borrower
-            "0x38f4F9aAf92bdCACDFe92eD1e025a66E343b05eC",  # Curve wstETH Lender crvUSD Borrower
+            "0xBc7998142b446beaE87cfd024A94320907eb64b1",  # Curve wstETH Lender crvUSD Borrower
         ],
         "ybold": [
             "0x2048A730f564246411415f719198d6f7c10A7961",  # yBOLD's WETH Strategy
